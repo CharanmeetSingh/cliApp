0eb1e0da059f809af3131382889e590f6d561dec
Charanmeet Singh <charanmeet95@gmail.com>
2021-04-24 16:47:55 +1000

    extracted DB configurations

--- user_upload.php
+++ user_upload.php
@@ -1,14 +1,39 @@
 <?php 
 
-if (isset($argv[1]) || isset($arg[2])) {
+if (isset($argv[1])) {
 	switch ($argv[1]) {
 		case "--help":
-			echo "--file: this is the name of the CSV file you want to parse.
-			\r\n--create_table: this will create MySQL users table.
-			\r\n--dry_run: this will run with --file directive in case you only want to run the script without creating any table.
+			echo "--file: The name of the CSV file you want to parse.
+			\r\n--create_table: Creates MySQL users table.
+			\r\n--dry_run: This will run with --file directive in case you only want to run the script without creating any table.
 			\r\n-u: MySQL username.
 			\r\n-p: MySQL password.
-			\r\n-h: MySQL host.";
+			\r\n-h: MySQL host";
+			break;
+		
+		case "-u":
+		case "-p":
+		case "-h":
+			$uname = "";
+			$pwd = "";
+			$host = "";
+			foreach ($argv as $key => $val) {
+				if ($val == "-u") {
+					$uname = $argv[$key + 1];
+				}
+				if ($val == "-p") {
+					$pwd = $argv[$key + 1];
+				}
+				if ($val == "-h") {
+					$host = $argv[$key + 1];
+				}
+			}
+			
+			// Make connection here
+			break;
+			
+		case "--create_table":
+			print_r($argv);
 			break;
 		
 		default:
