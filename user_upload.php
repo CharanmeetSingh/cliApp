fbe5f28ad6a15f6fe1e3133a5b5e0419f0cd7f07
Charanmeet Singh <charanmeet95@gmail.com>
2021-04-25 17:49:00 +1000

    setup connection and create table dir

--- user_upload.php
+++ user_upload.php
@@ -1,5 +1,20 @@
 <?php 
 
+function connection() {  		// creates connection
+	$config = json_decode(file_get_contents('config.php'), TRUE);
+	if (is_null($config)) {
+		echo "Please first specify username, password and host for DB connection.";
+	}else {
+		$conn = new mysqli($config[0], $config[1], $config[2], $config[3]);   //config[] includes db info uname, pwd, host and dbname
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		return $conn;
+	}
+}
+
+
 if (isset($argv[1])) {
 	switch ($argv[1]) {
 		case "--help":
@@ -8,15 +23,14 @@ if (isset($argv[1])) {
 			\r\n--dry_run: This will run with --file directive in case you only want to run the script without creating any table.
 			\r\n-u: MySQL username.
 			\r\n-p: MySQL password.
-			\r\n-h: MySQL host";
+			\r\n-h: MySQL host
+			\r\n-db: MySQL DB";
 			break;
 		
 		case "-u":
 		case "-p":
 		case "-h":
-			$uname = "";
-			$pwd = "";
-			$host = "";
+		case "-db":
 			foreach ($argv as $key => $val) {
 				if ($val == "-u") {
 					$uname = $argv[$key + 1];
@@ -27,13 +41,37 @@ if (isset($argv[1])) {
 				if ($val == "-h") {
 					$host = $argv[$key + 1];
 				}
+				if ($val == "-db") {
+					$db = $argv[$key + 1];
+				}
 			}
 			
-			// Make connection here
+			// save db configurations
+			if (isset($uname) && isset($pwd) && isset($host) && isset($db)) {
+				$config = [$host, $uname, $pwd, $db];
+				$content = json_encode($config);
+				file_put_contents('config.php', $content);
+				echo "configurations saved successfully.";
+			}else{
+				echo "Please specify username, password, host and DB for connection, all in one go.";
+			}	
 			break;
 			
 		case "--create_table":
-			print_r($argv);
+			// sql to create table
+			$sql = "CREATE TABLE users (
+			name VARCHAR(30) NOT NULL,
+			surname VARCHAR(30) NOT NULL,
+			email VARCHAR(50) PRIMARY KEY,
+			reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
+			$conn = connection();
+			if (isset($conn)) {
+				if ($conn->query($sql) === TRUE) {
+					echo "Table users created successfully";
+				} else {
+					echo "Error creating table: " . $conn->error;
+				}
+			}
 			break;
 		
 		default:
