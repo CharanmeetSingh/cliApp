9cf50748ac6ec04b6a3b7ebeab4b59c9fd4b41fa
Charanmeet Singh <charanmeet95@gmail.com>
2021-04-27 00:58:36 +1000

    setup validations and insertion

--- user_upload.php
+++ user_upload.php
@@ -1,11 +1,11 @@
 <?php 
 
 function connection() {  		// creates connection
-	$config = json_decode(file_get_contents('config.php'), TRUE);
+	$config = json_decode(file_get_contents('config.php'), TRUE); //config[] includes db info: uname, pwd, host and dbname.
 	if (is_null($config)) {
 		echo "Please first specify username, password and host for DB connection.";
 	}else {
-		$conn = new mysqli($config[0], $config[1], $config[2], $config[3]);   //config[] includes db info uname, pwd, host and dbname
+		$conn = new mysqli($config[0], $config[1], $config[2], $config[3]);
 		// Check connection
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
@@ -20,7 +20,7 @@ if (isset($argv[1])) {
 		case "--help":
 			echo "--file: The name of the CSV file you want to parse.
 			\r\n--create_table: Creates MySQL users table.
-			\r\n--dry_run: This will run with --file directive in case you only want to run the script without creating any table.
+			\r\n--dry_run: This will run with --file directive in case you only want to run the script without making any entry in DB. Ex. --file [fileName] --dry_run.
 			\r\n-u: MySQL username.
 			\r\n-p: MySQL password.
 			\r\n-h: MySQL host
@@ -32,7 +32,7 @@ if (isset($argv[1])) {
 		case "-h":
 		case "-db":
 			foreach ($argv as $key => $val) {
-				if ($val == "-u") {
+				if ($val == "-u") { 
 					$uname = $argv[$key + 1];
 				}
 				if ($val == "-p") {
@@ -46,31 +46,96 @@ if (isset($argv[1])) {
 				}
 			}
 			
-			// save db configurations
+			// Saving db configurations
 			if (isset($uname) && isset($pwd) && isset($host) && isset($db)) {
 				$config = [$host, $uname, $pwd, $db];
 				$content = json_encode($config);
 				file_put_contents('config.php', $content);
 				echo "configurations saved successfully.";
 			}else{
-				echo "Please specify username, password, host and DB for connection, all in one go.";
+				echo "Please specify username, password, host and DB for connection, Ex. -u [uname] -p [pwd] and so forth.";
 			}	
 			break;
 			
 		case "--create_table":
+			$conn = connection(); // Creating connection
 			// sql to create table
 			$sql = "CREATE TABLE users (
 			name VARCHAR(30) NOT NULL,
 			surname VARCHAR(30) NOT NULL,
-			email VARCHAR(50) PRIMARY KEY,
-			reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
-			$conn = connection();
-			if (isset($conn)) {
-				if ($conn->query($sql) === TRUE) {
-					echo "Table users created successfully";
-				} else {
-					echo "Error creating table: " . $conn->error;
+			email VARCHAR(50) PRIMARY KEY)";
+			
+			if ($conn->query($sql) === TRUE) {
+				echo "Table users created successfully";
+			} else {
+				echo "Error creating table: " . $conn->error;
+			}
+			break;
+			
+		case "--file":
+			if (isset($argv[2]) && !isset($argv[3])) {
+				$conn = connection(); // Creating connection
+				$file = fopen($argv[2], 'r');
+				while ($row = fgetcsv($file)) {
+					if ($row[0] != "name" && $row[1] != "surname" && $row[2] != "email") {  // Omitting column names
+						// Insertion into table
+						if (filter_var(trim($row[2]), FILTER_VALIDATE_EMAIL)) { // Validating emails
+							$name = ucfirst(strtolower($row[0]));
+							$name = trim(trim($name), '!'); // First trim for whitespace then second for !
+							$name = $conn->real_escape_string($name); // Escaping name for special char.
+							$surname = trim(ucfirst(strtolower($row[1])));
+							$surname = $conn->real_escape_string($surname); // Escaping surname for special char.
+							$email = trim(strtolower($row[2]));
+							$email = $conn->real_escape_string($email); // Escaping email for special char.
+							$sql = "INSERT INTO users (name, surname, email)
+							VALUES ('$name', '$surname', '$email')";
+							
+							if ($conn->query($sql) === TRUE) {
+							echo "\r\n New record created successfully";
+							} else {
+								echo "\r\n Error: " . $conn->error;
+							}
+						}else {
+							echo "\r\n Insertion failed due to invalid email.";
+						}
+					}
 				}
+				fclose($file);
+			}else {
+				if (!isset($argv[3])) {
+					echo "Please specify the name of the file to be parsed.";
+				}
+			}
+			
+			if (isset($argv[3]) && $argv[3] == "--dry_run") {
+				$file = fopen($argv[2], 'r');
+				while ($row = fgetcsv($file)) {
+					if ($row[0] != "name" && $row[1] != "surname" && $row[2] != "email") {  // Omitting column names
+						// Insertion into table
+						if (filter_var(trim($row[2]), FILTER_VALIDATE_EMAIL)) { // Validating emails
+							$name = ucfirst(strtolower($row[0]));
+							$name = trim(trim($name), '!'); // First trim for whitespace then second for !
+							$name = $conn->real_escape_string($name); // Escaping name for special char.
+							$surname = trim(ucfirst(strtolower($row[1])));
+							$surname = $conn->real_escape_string($surname); // Escaping surname for special char.
+							$email = trim(strtolower($row[2]));
+							$email = $conn->real_escape_string($email); // Escaping email for special char.
+							/* $sql = "INSERT INTO users (name, surname, email)
+							VALUES ('$name', '$surname', '$email')";
+							
+							if ($conn->query($sql) === TRUE) {
+							echo "\r\n New record created successfully";
+							} else {
+								echo "\r\n Error: " . $conn->error;
+							} */
+						}else {
+							echo "\r\n Insertion failed due to invalid email.";
+						}
+					}
+				}
+				fclose($file);
+			}else {
+				echo "type --help to see options.";
 			}
 			break;
 		
